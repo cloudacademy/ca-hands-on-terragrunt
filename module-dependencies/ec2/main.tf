39b8c9736343a8514bb0b3c78ce78a1b8e926c7c
orellanaluke <67909347+orellanaluke@users.noreply.github.com>
2020-11-25 14:48:50 -0500

    Update main.tf

--- module-dependencies/ec2/main.tf
+++ module-dependencies/ec2/main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t3.nano"
   subnet_id = var.subnet_id
   vpc_security_group_ids = [var.vpc_sg]
 
